a79c5e820c66912b91b4742881e8fd06e2ec4f45
pornniteesir <pornniteesir@gosoft.co.th>
2024-11-14 13:24:54 +0700

    Create api

---
+++ src/__tests__/app.test.ts
@@ -0,0 +1,32 @@
+import request from "supertest";
+import { app } from "../index";
+
+describe("GET /api/data", () => {
+  it("should return data with dynamic properties and valid values", async () => {
+    const response = await request(app).get("/api/data");
+
+    expect(response.status).toBe(200);
+
+    const responseKeys = Object.keys(response.body);
+
+    expect(responseKeys.length).toBeGreaterThan(0);
+
+    responseKeys.forEach((key) => {
+      const department = response.body[key];
+
+      expect(department).toHaveProperty("male");
+      expect(department).toHaveProperty("female");
+      expect(department).toHaveProperty("ageRange");
+      expect(department).toHaveProperty("hair");
+      expect(department).toHaveProperty("addressUser");
+
+      expect(department.male).toBeGreaterThanOrEqual(0);
+      expect(department.female).toBeGreaterThanOrEqual(0);
+
+      expect(department.ageRange).toMatch(/^\d{2}-\d{2}$/);
+
+      expect(Object.keys(department.hair).length).toBeGreaterThan(0);
+      expect(Object.keys(department.addressUser).length).toBeGreaterThan(0);
+    });
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,56 @@
+import express, { Request, Response } from "express";
+import axios from "axios";
+
+export const app = express();
+const PORT = 3000;
+
+app.get("/api/data", async (req: Request, res: Response) => {
+  try {
+    const response = await axios.get("https://dummyjson.com/users");
+    const { users } = response.data;
+    const groupedItems = users.reduce((accumulator: any, user: any) => {
+      const { department } = user.company;
+      if (!accumulator[department]) {
+        accumulator[department] = {
+          male: 0,
+          female: 0,
+          ageRange: "",
+          hair: {},
+          addressUser: {},
+        };
+      }
+
+      accumulator[department][user.gender] =
+        (accumulator[department][user.gender] || 0) + 1;
+
+      accumulator[department]["hair"][user.hair.color] =
+        (accumulator[department]["hair"][user.hair.color] || 0) + 1;
+
+      accumulator[department]["addressUser"][user.firstName + user.lastName] =
+        user.address.postalCode;
+
+      const currentAge = accumulator[department]["ageRange"];
+      const mergedAgeRange = [
+        ...currentAge
+          .split("-")
+          .filter((str: string) => str !== "" && Number(str)),
+        user.age,
+      ];
+      accumulator[department]["ageRange"] = `${Math.min(
+        ...mergedAgeRange
+      )}-${Math.max(...mergedAgeRange)}`;
+      return accumulator;
+    }, {});
+
+    app.set("json spaces", 2);
+    res.json(groupedItems);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching data", error });
+  }
+});
+
+if (process.env.NODE_ENV !== "test") {
+  app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+  });
+}
